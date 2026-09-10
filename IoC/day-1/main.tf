abf71191a31fc10734236c5ad4146ea4566de565
fahad-md-kamal <fahadmdkamal@gmail.com>
2026-09-10 09:30:51 +0600

    terraform configuration

---
+++ IoC/day-1/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "ap-south-1"
+  profile = "fahad"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name  = "learn-terraform"
+    Owner = "fahad"
+    Event = "Learning"
+  }
+}
